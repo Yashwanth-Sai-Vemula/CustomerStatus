f85f86718a6d8fbe81bc72237442d4a113082518
vemula.yashwanth <vemula.yashwanth@TEKFRIDAY282.com>
2024-09-04 11:53:12 +0530

    #Added new model for storing bar graph data

--- Model/Customer.cs
+++ Model/Customer.cs
@@ -1,5 +1,12 @@
 ﻿namespace CustomerStatus.Model
 {
+
+    public class CombinedDataModel
+    {
+        public List<Customer> customerData { get; set; }
+        public List<BarData> ApplicationData { get; set; }
+
+    }
     public class Customer
     {
         public int CustomerId { get; set; }
@@ -11,5 +18,10 @@
         public int ApplicationStatusCode { get; set; }
         public DateTime InsertedDate { get; set; }
         public string Color { get; set; }
+    }
+    public class BarData
+    {
+        public int ApplicationStatusCode { get; set; }
+        public int Count { get; set; }
     }
 }
